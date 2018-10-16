f3696307d8b2ae34713ebeff5ca4cf475ca07c20
wantero <wantero@dlctec.com.br>
2018-10-16 10:13:08 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'totvs-thf';
+
+  menus = [
+    { label: 'Hello World', link: './hello-world' },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ThfModule } from '@totvs/thf-ui';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloWorldComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ThfModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
